ae11c5aa62c0c0f5d4e456c73bb8814d62d881b1
Kashi1510 <kashinaruni@gmail.com>
2025-03-09 13:44:14 +0530

    Dashboard created

--- Internship-Portal/src/app/app.routes.ts
+++ Internship-Portal/src/app/app.routes.ts
@@ -5,13 +5,16 @@ import { StudentDashboardComponent } from './pages/student-dashboard/student-das
 import { EmployerDashboardComponent } from './pages/employer-dashboard/employer-dashboard.component';
 import { InternshipListComponent } from './pages/internship-list/internship-list.component';
 import { InternshipDetailsComponent } from './pages/internship-details/internship-details.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
-    {path:'',component:LoginComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path:'signup',component:SignupComponent},
     {path:'student-dashboard',component:StudentDashboardComponent},
     {path:'employer-dashboard',component:EmployerDashboardComponent},
     {path:'internships',component:InternshipListComponent},
-    {path:'internship/id',component:InternshipDetailsComponent}
+    {path:'internship/id',component:InternshipDetailsComponent},
+    {path:'home',component:HomeComponent}
 ];
