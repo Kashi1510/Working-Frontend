1781f71984dff80dcf52c6bb0f9466eb69e2506c
Kashi1510 <kashinaruni@gmail.com>
2025-03-12 16:47:03 +0530

    frontend changed

--- Internship-Portal/src/app/app.component.ts
+++ Internship-Portal/src/app/app.component.ts
@@ -11,5 +11,6 @@ import { Router } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  title: any;
   
 }
--- Internship-Portal/src/main.ts
+++ Internship-Portal/src/main.ts
@@ -5,6 +5,5 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 
-bootstrapApplication(AppComponent,{
-  providers: [provideRouter(routes)]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent,appConfig)
+.catch((err) => console.error(err));
